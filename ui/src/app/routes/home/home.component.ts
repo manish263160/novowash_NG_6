5ac3b60015d8aa8cf9635bb4c6d75a44d073dac9
Ramraj Chauhan <ramang@dewsolutions.in>
2018-07-16 00:15:42 +0530

    for home service list

--- ui/src/app/routes/home/home.component.ts
+++ ui/src/app/routes/home/home.component.ts
@@ -18,12 +18,38 @@ export class HomeComponent implements OnInit, OnDestroy {
         {img: "http://placehold.it/350x150/666666"},
         {img: "http://placehold.it/350x150/777777"}
     ];
+    public serviceItems = [{
+            icon: "url('/assets/img/home-cleaning.png')",
+            name: "Home Cleaning"
+        }, {
+            icon: "url('/assets/img/furniture.png')",
+            name: "Home Upholstery/Furniture"
+        }, {
+            icon: "url('/assets/img/home-package.png')",
+            name: "New home package"
+        }, {
+            icon: "url('/assets/img/sanitization.png')",
+            name: "Home Sanitization"
+        }, {
+            icon: "url('/assets/img/car-detailing.png')",
+            name: "Car Detailing"
+        }, {
+            icon: "url('/assets/img/laundry.png')",
+            name: "Laundry Services"
+        }, {
+            icon: "url('/assets/img/pest-control.png')",
+            name: "Pest Control"
+        }, {
+            icon: "url('/assets/img/comm-cleaning.png')",
+            name: "Commercial cleaning"
+    }];
     public cities = [
         {value: 'ncr-0', viewValue: 'Delhi-NCR'},
         {value: 'mumbai-1', viewValue: 'Mumbai'},
         {value: 'bangalore-2', viewValue: 'Bangalore'}
     ];
     public slideConfig = {"slidesToShow": 4, "slidesToScroll": 1};
+    public slideHListConfig = {"slidesToShow": 6, "slidesToScroll": 1};
     
     public afterChange(e) {
         console.log('afterChange');
