604b06d91f2fb71cb602f7d848fdd763e10750f6
Raman Goel <ramangoel97@gmail.com>
2018-08-22 14:50:16 +0530

    For prod env

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 declare const require: any;
 export const environment = {
   auth: {
-    clientId: "web",
-    clientSecret: "secret",
+    clientId: "novo-client",
+    clientSecret: "novo-secret",
     grantType: "password",
     tokenUrl: "http://13.59.141.30:8080/NovoWash/oauth/token",
+    otpUrl: "http://13.59.141.30:8080/NovoWash/login/generateOtp",
   },
   production: true,
   version: require("../../package.json").version,
