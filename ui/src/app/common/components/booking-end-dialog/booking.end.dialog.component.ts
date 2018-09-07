54f27b1e90c9decef6f278067eec9621a0f66685
Raman Goel <ramangoel97@gmail.com>
2018-09-07 21:38:29 +0530

    for booking end

---
+++ ui/src/app/common/components/booking-end-dialog/booking.end.dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, OnInit, Input, Output, ViewEncapsulation } from "@angular/core";
+import { ROPCService } from "../../../../app/auth/ropc.service";
+
+@Component({
+    encapsulation: ViewEncapsulation.None,
+    // tslint:disable-next-line:use-host-property-decorator
+    selector: "booking-end-dialog",
+    styleUrls: ["../booking.dialog.component.scss"],
+    templateUrl: "booking.end.dialog.component.html",
+})
+export class BookingEndDialogComponent implements OnInit {
+    @Input() public isSuccess: boolean;
+    @Input() public totalAmount: any;
+    @Output() public onBookingEndClosed: EventEmitter<any> = new EventEmitter();
+
+    constructor(
+        private ropcService: ROPCService,
+    ) {}
+
+    ngOnInit() {}
+
+    public closeDialog() {
+        this.onBookingEndClosed.emit();
+    }
+}
